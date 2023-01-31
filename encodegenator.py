667dd5e16da029512646c104cf46b4715edeb89d
SRIRAM <104987164+SriramKalai@users.noreply.github.com>
2023-01-31 21:04:25 +0530

    Update encodegenator.py
    remove friebase

--- encodegenator.py
+++ encodegenator.py
@@ -8,8 +8,8 @@ from firebase_admin import db
 from firebase_admin import storage
 cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred,{
-    'databaseURL':"https://faceattedance-default-rtdb.firebaseio.com/",
-    'storageBucket':"faceattedance.appspot.com"
+    'databaseURL':"",
+    'storageBucket':""
 })
 
 
